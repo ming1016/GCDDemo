4170d2952e96d0be2c72f50a7399e5300b768ff0
daiming <daiming@didichuxing.com>
2016-01-13 17:10:04 +0800

    first commit

---
+++ GCDDemo/GCDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDDemo
+//
+//  Created by DaiMing on 16/1/13.
+//  Copyright © 2016年 Starming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDDemo/GCDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDDemo
+//
+//  Created by DaiMing on 16/1/13.
+//  Copyright © 2016年 Starming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
